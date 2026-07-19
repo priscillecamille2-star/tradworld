00dc81f2e2a6fd0f2876a90f404dd593f0ff428c
priscillecamille2-star <priscillecamille2@gmail.com>
2026-07-19 12:22:28 +0000

    feat: add main entry point

---
+++ src/index.js
@@ -0,0 +1,17 @@
+// Tradworld - Entry point
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/../public/index.html");
+});
+
+app.listen(PORT, () => {
+  console.log(`Tradworld running on port ${PORT}`);
+});
+
+module.exports = app;
